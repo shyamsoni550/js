f4479bdd2a710ecc368befd1173a03c76195fd2a
shyamsoni550 <vayashyam550@gmail.com>
2024-02-13 01:12:40 +0530

    all about object

--- classes_and_oop/object.js
+++ classes_and_oop/object.js
@@ -1,1 +1,27 @@
-function
+// function multiplayby5(num)
+// {
+//      return num*5
+// }
+
+// multiplayby5.power=2;
+// console.log(multiplayby5(5));
+// console.log(multiplayby5.power);
+// console.log(multiplayby5.prototype);
+
+function createuser(username,score) {
+     this.username=username;
+     this.score=score;
+}
+
+createuser.prototype.increment=()=>{
+     this.score++;
+}
+createuser.prototype.printme=()=>
+{
+     console.log(`price is ${this.score}`);
+}
+
+const chai=new createuser("chai",25);
+const tea=createuser("tea",250);
+
+chai.printme()
